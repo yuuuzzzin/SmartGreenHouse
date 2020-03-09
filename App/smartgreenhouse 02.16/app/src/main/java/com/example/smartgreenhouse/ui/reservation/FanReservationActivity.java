bff346e83c63f9db76d3f076e266d3313f2ac49c
yuuuzzzin <uiuiui118437@gmail.com>
2020-03-10 02:31:26 +0900

    Upload App Version 02.16
    Upload App Version 02.16

---
+++ App/smartgreenhouse 02.16/app/src/main/java/com/example/smartgreenhouse/ui/reservation/FanReservationActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartgreenhouse.ui.reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartgreenhouse.R;
+
+public class FanReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fan_reservation);
+    }
+}
