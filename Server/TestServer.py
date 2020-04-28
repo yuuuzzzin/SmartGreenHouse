e5c354e65ff27600cfc6e6c1b0c86227851c9ffb
jeongmin78 <48047620+jeongmin78@users.noreply.github.com>
2020-04-28 17:11:29 +0900

    upload
    led, fan 예약 작동 기능 완성

--- Server/TestServer.py
+++ Server/TestServer.py
@@ -67,6 +67,28 @@ def userCheck(): #사용자가 물줘라 하면 워터펌프 작동
         user_l = row[4]
         user_h = row[5]
         #print(user_s1)
+
+        curs = conn.cursor()
+        sql = """select * from led order by id desc limit 1""" #마지막 예약
+        curs.execute(sql)
+        row = curs.fetchone()
+        Start_led = row[1]
+        End_led = row[2]
+
+        curs = conn.cursor()
+        sql = """select * from fan order by id desc limit 1"""
+        curs.execute(sql)
+        row = curs.fetchone()
+        Start_fan = row[1]
+        End_fan = row[2]
+
+        Current = datetime.datetime.now()+timedelta(hours=9)
+        CurrentTime = Current.strftime('%H:%M') #str형식으로 변환
+        
+        print(Start_led, End_led, Start_fan, End_fan, CurrentTime)
+        
+        if (Start_led == CurrentTime):
+            print("같다")
         global s1,s2,s3,l,h
         if (user_s1 == 'on'):
             s1 = "StartMotor1"
@@ -74,13 +96,13 @@ def userCheck(): #사용자가 물줘라 하면 워터펌프 작동
             s2 = "StartMotor2"
         if (user_s3 == 'on'):
             s3 = "StartMotor3"
-        if (user_l == 'on'):
+        if (user_l == 'on' or Start_led == CurrentTime):
             l = "StartLed"
-        if (user_l == 'off'):
+        if (user_l == 'off' or End_led == CurrentTime):
             l == "StopLed"
-        if (user_h == 'on'):
+        if (user_h == 'on' or Start_fan == CurrentTime):
             h = "StartFan"
-        if (user_h == 'off'):
+        if (user_h == 'off' or End_fan == CurrentTime):
             h = "StopFan"
         print(s1,s2,s3,l,h)
 
@@ -121,38 +143,37 @@ def updateDB(s1, s2, s3, l, h): #DB에 작동 상황 저장
     clientSocekt.send(bytes('%s,%s,%s,%s,%s'%(s2,s3,l,h,s1),"UTF-8")) #순서가 이상함
     #print(s1, s2, s3, l, h ,"send data success!!")
     
-def CurrentSeason(month): #현재 달에 맞는 계절
-    if month == '02' or month == '01' or month =='12':
-        return watercycleWinterCode
-    elif month == '03' or month == '04' or month == '05':
-        return watercycleSprngCode
-    elif month == '06' or month == '07' or month == '08':
-        return watercycleSummerCode
-    elif month == '09' or month == '10' or month == '11':
-        return watercycleAutumnCode
+##def CurrentSeason(month): #현재 달에 맞는 계절
+##    if month == '02' or month == '01' or month =='12':
+##        return watercycleWinterCode
+##    elif month == '03' or month == '04' or month == '05':
+##        return watercycleSprngCode
+##    elif month == '06' or month == '07' or month == '08':
+##        return watercycleSummerCode
+##    elif month == '09' or month == '10' or month == '11':
+##        return watercycleAutumnCode
 
 def WaterCycle(watercycleCode, soil):
     if watercycleCode == "053001":
-        if soil > 1000 : #임시값이므로 실험을 통한 수정 필요
+        if soil < 550 : #물 준 직후 50까지 떨어졌다가 450대
             return "soilgood"
         else:
-            return "StartMotor@@"
+            return "StartMotor"
     elif watercycleCode == "053002":
-        if soil > 800 : #임시값이므로 실험을 통한 수정 필요
+        if soil < 750 : 
             return "soilgood"
         else:
-            return "StartMotor@@"
+            return "StartMotor"
     elif watercycleCode == "053003":
-        if soil > 600 : #임시값이므로 실험을 통한 수정 필요
+        if soil < 950 : 
             return "soilgood"
         else:
-                            #print("soilbad3")
-            return "StartMotor@@"
+            return "StartMotor"
     elif watercycleCode == "053004":
-        if soil > 400 : #임시값이므로 실험을 통한 수정 필요
+        if soil < 1150 : 
             return "soilgood"
         else:
-            return "StartMotor@@"
+            return "StartMotor"
 
 def Humi(humi):
     if humi >= 70 :
@@ -301,12 +322,13 @@ def TTTT(): #이름바꾸기
         watercycleCode = json_data["watercycleWinterCode"]
     s3 = WaterCycle(watercycleCode, soil3) + '3'
     
+#############################################################################
+
 sched = BackgroundScheduler()
 sched.start()
 
-#sched.add_job(recvData,'interval', seconds=5, id="test_1", args=[]) #5초에 한번
-sched.add_job(TTTT,'cron', second='1', id="test_1", args=[])
-sched.add_job(SensorUpload,'cron', second='30', id="test_2", args=[]) #1분에 한번
+sched.add_job(TTTT,'cron', second='1', id="test_1", args=[]) #1분에 한번
+sched.add_job(SensorUpload,'cron', second='31', id="test_2", args=[]) #1분에 한번
 
 while True:
     try:
