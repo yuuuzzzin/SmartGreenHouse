fae828a14e2ea2757b0d93af340028811144be13
yuuuzzzin <uiuiui118437@gmail.com>
2021-03-29 16:01:18 +0900

    Delete

--- Android/smartgreenhouse 01.28/app/src/main/java/com/example/smartgreenhouse/ui/camera/CameraViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartgreenhouse.ui.camera;
-
-import androidx.lifecycle.ViewModel;
-
-public class CameraViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- Android/smartgreenhouse 02.14/app/src/main/java/com/example/smartgreenhouse/ui/plantstatus/PlantStatusViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartgreenhouse.ui.plantstatus;
-
-import androidx.lifecycle.ViewModel;
-
-public class PlantStatusViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
